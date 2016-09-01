4bdb0d9f3e2baad14a98dafa9aa21686217a243f
Augusto Tibalt <augusto.tibalt@hotmail.com>
2016-09-01 01:05:00 -0300

    Funciona la calculadora y estan puestos los iconos. Faltarian hacer las validaciones de los input y algun manejo de excepciones.

--- app/src/main/java/mandarine/dam/isi/frsf/utn/edu/ar/lab01c2016/MainActivity.java
+++ app/src/main/java/mandarine/dam/isi/frsf/utn/edu/ar/lab01c2016/MainActivity.java
@@ -1,25 +1,28 @@
 package mandarine.dam.isi.frsf.utn.edu.ar.lab01c2016;
 
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements OnSeekBarChangeListener {
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, Button.OnClickListener {
 
     SeekBar slider;
+    Button confButton;
+    TextView aCobrar;
+    TextView resultado;
+    EditText montoIngresado;
     double intereses;
     int cantDias;
     double capital;
     TextView textoSlider;
-
-    public void calculadora(int cantDias,double capital){
-
-
-    }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +31,19 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
 
         slider = (SeekBar)findViewById(R.id.seekBar);
         textoSlider = (TextView)findViewById(R.id.textSlider);
+        confButton = (Button)findViewById(R.id.button);
+        aCobrar = (TextView)findViewById(R.id.intereses);
+        resultado = (TextView)findViewById(R.id.resultado);
+        montoIngresado = (EditText)findViewById(R.id.inputImporte);
 
         slider.setOnSeekBarChangeListener(this);
+        confButton.setOnClickListener(this);
     }
 
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        cantDias = progress;
-        textoSlider.setText(cantDias);
+        textoSlider.setText(String.valueOf(Integer.valueOf(progress)));
     }
 
     @Override
@@ -44,4 +51,56 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {   }
+
+    @Override
+    public void onClick(View v) {
+
+        String resPlazoFijo = calculadora();
+        aCobrar.setText("$"+resPlazoFijo);
+        aCobrar.setVisibility(v.VISIBLE);
+        resultado.setText(getString(R.string.exito, resPlazoFijo));
+        resultado.setVisibility(v.VISIBLE);
+        resultado.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.Verde));
+
+
+    }
+
+    public String calculadora() {
+
+        cantDias = slider.getProgress();
+        capital = Integer.parseInt(montoIngresado.getText().toString());
+
+        Double tasa = getTasa(cantDias, capital);
+
+        intereses = getInteres(capital, tasa, cantDias);
+
+
+        return String.valueOf(intereses);
+    }
+
+    private double getInteres(double capital, Double tasa, int cantDias ) {
+        double intereses;
+
+        intereses = capital*((Math.pow(1+tasa,(double) cantDias/360)-1));
+        DecimalFormat df = new DecimalFormat("#.##");
+        intereses = Double.valueOf(df.format(intereses));
+
+        return intereses;
+    }
+
+    @NonNull
+    private Double getTasa(int cantDias, double capital) {
+        String tasa = null;
+        if(capital<5000){
+            tasa = cantDias < 30 ?   getString(R.string.menor30menos5k) : getString(R.string.mayig30menos5k);
+        }
+        else if(capital>5000 && capital<99999 ){
+            tasa =  cantDias < 30 ?   getString(R.string.menor30entre5y99k) : getString(R.string.mayig30entre5y99k);
+        }
+        else if(capital>99999 ){
+            tasa =  cantDias < 30 ?   getString(R.string.menor30mas99k) : getString(R.string.mayig30mas99k);
+        }
+        return Double.parseDouble(tasa);
+    }
+
 }
